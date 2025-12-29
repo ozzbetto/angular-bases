9889c7b70bfd6a175dcf7bccbc651a4e893aba02
ozzbetto <segovia.al@gmail.com>
2025-12-29 19:34:16 -0300

    Primeras bases de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.modules';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk','She Hulk', 'Thor'];
 
+  public deletedHero?: string = '';
+
+  // Delete last item
+  public deleteLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
