b2dc2f43ec461d22c24ca9d26c7f321e37178d15
ozzbetto <segovia.al@gmail.com>
2026-01-03 23:34:44 -0300

    ngClass basado en condiciones

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
 
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  },]
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,6 +1,7 @@
 
 
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -9,5 +10,14 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent {
-
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  }, {
+    name: 'Goku',
+    power: 9500
+  }, {
+    name: 'Vegeta',
+    power: 7500
+  }];
 }
