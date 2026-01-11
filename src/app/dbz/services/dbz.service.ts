0975d22138bd2636e0c7d7e1e13f355e6d264af2
ozzbetto <segovia.al@gmail.com>
2026-01-10 22:36:16 -0300

    uuid agregado

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,22 +1,34 @@
 
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 import { Character } from '../interfaces/character.interface';
+
 
 @Injectable({providedIn: 'root'})
 export class DbzService {
   public characters: Character[] = [{
+    id: uuid(),
     name: 'Krillin',
     power: 1000
   }, {
+    id: uuid(),
     name: 'Goku',
     power: 9500
   }, {
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
 
   onNewCharacter( character: Character): void {
-    this.characters.push( character );
+
+    const newCharacter: Character = {
+      id: uuid(),
+      name: character.name,
+      power: character.power
+    }
+
+    this.characters.push( newCharacter );
   }
 
   onDeleteCharacter( index: number ): void {
